d7a7b9908753f9c32de77dad0798afc2f98a23b0
avraham123 <40668073+avraham123@users.noreply.github.com>
2023-04-26 19:24:25 +0300

    Add files via upload

--- product.js
+++ product.js
@@ -10,3 +10,19 @@ window.onscroll = () => {
     menu.classList.remove('bx-x');
     navbar.classList.remove('active'); 
 }
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("back-to-top-btn").style.display = "block";
+  } else {
+    document.getElementById("back-to-top-btn").style.display = "none";
+  }
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
