b964d235add392094f91e9f6c83ca5f63ec18d49
Avraham Life <40668073+avraham123@users.noreply.github.com>
2023-08-13 20:45:42 +0300

    Add files via upload

--- product.js
+++ product.js
@@ -26,3 +26,82 @@ function scrollToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+function isElementInViewport(el, offset) {
+  var rect = el.getBoundingClientRect();
+  return (
+      rect.top + offset <= (window.innerHeight || document.documentElement.clientHeight)
+  );
+}
+
+function handleScroll() {
+  var aboutSection = document.querySelector('.about');
+  
+  if (isElementInViewport(aboutSection, window.innerHeight * 0.3)) {
+      aboutSection.classList.add('fade-in');
+  }
+}
+
+window.addEventListener('scroll', handleScroll);
+window.addEventListener('load', handleScroll); // To handle initial page load
+
+
+
+$(document).ready(function() {
+  var header = $(".header");
+  var headerHeight = header.height();
+  var windowHeight = $(window).height();
+
+  $(window).scroll(function() {
+      var scrollTop = $(this).scrollTop();
+
+      if (scrollTop > header.offset().top - windowHeight + headerHeight) {
+          header.addClass("active");
+      } else {
+          header.removeClass("active");
+      }
+  });
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  const boxes = document.querySelectorAll(".box");
+  const options = {
+      rootMargin: "-50px", // Adjust as needed
+      threshold: 0.2 // Adjust as needed
+  };
+
+  const observer = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              entry.target.classList.add("active");
+              observer.unobserve(entry.target);
+          }
+      });
+  }, options);
+
+  boxes.forEach(box => {
+      observer.observe(box);
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  const boxes = document.querySelectorAll(".box, .box1"); // Add .box1 here
+  const options = {
+      rootMargin: "-50px", // Adjust as needed
+      threshold: 0.2 // Adjust as needed
+  };
+
+  const observer = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              entry.target.classList.add("active");
+              observer.unobserve(entry.target);
+          }
+      });
+  }, options);
+
+  boxes.forEach(box => {
+      observer.observe(box);
+  });
+});
